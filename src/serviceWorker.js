d7b0d1e4a4203d1b955bd7544ac1d3854cd506db
zedPai <weichih.pai@imersive.com>
2019-10-08 05:11:48 +0800

    Remove useless code

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -75,7 +75,6 @@ function registerValidSW(swUrl, config) {
                 'New content is available and will be used when all ' +
                 'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
               );
-              window.location.reload();
               // Execute callback
               if (config && config.onUpdate) {
                 config.onUpdate(registration);
